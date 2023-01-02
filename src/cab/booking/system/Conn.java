87582006a11b2fa010c4a15b6a2ec84ddc992551
Devendra Johari <54894480+DevendraJohari24@users.noreply.github.com>
2023-01-02 22:05:53 +0530

    commit

---
+++ src/cab/booking/system/Conn.java
@@ -0,0 +1,23 @@
+
+package cab.booking.system;
+
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///cbs1","root","mysql123"); 
+            
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
+
